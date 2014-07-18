d0c310ca266b0793ef75e24cb88c03272e51b4b5
Marek Kaszewski <marek.kaszewski@gmail.com>
2014-07-18 19:16:20 +0100

    Adding new services

--- lib/src/service/jhub1service.dart
+++ lib/src/service/jhub1service.dart
@@ -8,16 +8,22 @@ import '../models/endpoint.dart';
 import '../models/endpoints.dart';
 import '../models/endpointtypes.dart';
 import '../models/delete.dart';
+import '../models/values.dart';
 import "package:restful/restful.dart";
 import 'request_wrapper.dart';
+import 'package:logging/logging.dart';
+
+Logger _logger = new Logger("restful.request_helper");
 
 class JHUB1OnlineServices extends Service {
   
   static const String RESOURCE_AGENTS = "agents";
   static const String RESOURCE_ENDPOINTS = "endpoints";
+  static const String RESOURCE_VALUES = "values";
   
   ModelTransform<Agent> agentAPI;
   ModelTransform<Agents> agentsAPI; 
+  ModelTransform<Values> valuesAPI;
   
   JHUB1OnlineServices() {
     var api = new RestApi(apiUri: "http://127.0.0.1:8081/mock0", format: new JsonFormat());
@@ -30,7 +36,12 @@ class JHUB1OnlineServices extends Service {
         api.resource(RESOURCE_AGENTS),
         (json) => new Agents()..createFromMap(json),
         (error) => new Agents()..setError(error)
-        );   
+        ); 
+    valuesAPI = new ModelTransform<Values>(
+        api.resource(RESOURCE_VALUES),
+        (json) => new Values()..createFromMap(json),
+        (error) => new Values()..setError(error)
+        );
   }
   
   /**
@@ -42,6 +53,7 @@ class JHUB1OnlineServices extends Service {
    */
   void getAgents(Function callback(Agents agents)) {
     agentsAPI.findAll().then((agents) => callback(agents));
+    _logger.warning("BLA");
   }
   
   /**
@@ -56,26 +68,37 @@ class JHUB1OnlineServices extends Service {
   }
   
   /**
-   * Retrives only new endpoints - the ones without assigned endpoint type.
-   * The call back provided returns response wrapper Endpoints which
+   * Retrives only values of new endpoints - the ones without assigned endpoint type.
+   * The call back provided returns response wrapper Values which
    * in case of error holds RequestFault object and has hasError() flag set 'true'.
    *
-   *     getNewEndopints((endpoints) => showNewEndpoints(endpoints));
+   *     getNewValues((values) => doSomethingWithMyValues(values));
    */
-  void getNewEndopints(Function callback(Endpoints endpoints)) {
+  void getNewValues(Function callback(Values values)) {
+    valuesAPI.findAll().then((values) => callback(values));
+  }
 
+  /**
+   * Retrives values of the all endpoints that belong to Agent identified by name.
+   * The call back provided returns response wrapper Values which
+   * in case of error holds RequestFault object and has hasError() flag set 'true'.
+   *
+   *     getValuesByAgentName((values) => doSomethingWithMyValues(values), "tt56rsnixh");
+   */
+  void getValuesByAgentName(Function callback(Values values), String agentName) {
+    valuesAPI.find(agentName).then((values) => callback(values));
   }
   
   /**
-   * Retrives all endpoints - the ones with assigned endpoint type.
-   * The call back provided returns response wrapper Endpoints which
+   * Retrives all endpoints that belong to Agent identified by name.
+   * The call back provided returns response wrapper Values which
    * in case of error holds RequestFault object and has hasError() flag set 'true'.
    *
    *     getEndopints((endpoints) => showEndpoints(endpoints));
    */
-  void getEndopints(Function callback(Endpoints endpoints)) {
-    
-  }
+ // void getEndopintsByAgentName(Function callback(Values values), String agentName) {
+ //   valuesAPI.find(agentName).then((values) => callback(values));
+ // }
   
   /**
    * Retrives endopint with requested ID.
@@ -84,13 +107,13 @@ class JHUB1OnlineServices extends Service {
    *
    *     getEndopintByID((agent) => showEndpointDetails(agent), "A-1E6B9-5P8TQ-HJ9JKDL0Y.733");
    */
-  void getEndopintByID(Function callback(Endpoint endpoint), String id) {
-    
-  }
+//  void getEndopintByID(Function callback(Endpoint endpoint), String id) {
+//    
+ // }
   
-  void getUserByID(Function callback(User user), String id) {
-    
-  }
+//  void getUserByID(Function callback(User user), String id) {
+ //   
+ // }
   
   /**
    * Retrives all endopint types: system defaults and defined by user.
@@ -99,17 +122,17 @@ class JHUB1OnlineServices extends Service {
    *
    *     getEndopintTypes((endpointTypes) => showEndpointTypes(endpointTypes));
    */
-  void getEndopintTypes(Function callback(EndpointTypes endpointTypes)) {
-    
-  }
+ // void getEndopintTypes(Function callback(EndpointTypes endpointTypes)) {
+ //   
+ // }
   
-  void putEndpoint(Function callback(Endpoint endpoint), Endpoint endpoint) {
+ // void putEndpoint(Function callback(Endpoint endpoint), Endpoint endpoint) {
     
-  }
+ // }
   
-  void putAgent(Function callback(Agent agent), Agent agent) {
+//  void putAgent(Function callback(Agent agent), Agent agent) {
     
-  }
+ // }
    
   /**
    * Deletes the agent with the agentID,
@@ -118,9 +141,9 @@ class JHUB1OnlineServices extends Service {
    *
    *     deleteAgent((delete) => confirmDeleted(delete), "A-1E6B9-5P8TQ-HJ9JKDL0Y.733");
    */
-  void deleteAgent(String agentID) {
+  //void deleteAgent(String agentID) {
     
-  }
+ // }
   
   /**
    * Deletes the endpoint with the endpointID,
@@ -129,10 +152,10 @@ class JHUB1OnlineServices extends Service {
    *
    *     deleteEndpoint((delete) => confirmDeleted(delete), "A-1E6B9-5P8TQ-HJ9JKDL0Y.733");
    */
-  void deleteEndpoint(Function callback(Delete delete), String endpointID) {
+ // void deleteEndpoint(Function callback(Delete delete), String endpointID) {
     
     
-  }
+ // }
 
   /**
    * Deletes the new endpoint with the newEndpointID,
@@ -141,8 +164,8 @@ class JHUB1OnlineServices extends Service {
    *
    *     deleteNewEndpoint((delete) => confirmDeleted(delete), "A-1E6B9-5P8TQ-HJ9JKDL0Y.733");
    */
-  void deleteNewEndpoint(Function callback(Delete delete), String newEndpointID) {
+  //void deleteNewEndpoint(Function callback(Delete delete), String newEndpointID) {
     
     
-  }
+  //}
 }
