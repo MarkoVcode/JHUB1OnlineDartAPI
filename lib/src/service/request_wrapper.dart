913da46b2b70d48c8221a7775fa4831675b417a7
Marek Kaszewski <marek.kaszewski@gmail.com>
2014-07-21 13:43:01 +0100

    adding new methods

--- lib/src/service/request_wrapper.dart
+++ lib/src/service/request_wrapper.dart
@@ -3,7 +3,7 @@ import "package:restful/restful.dart";
 import 'dart:async';
 
 typedef Object ModelFactory(Object response);
-typedef Object ErrorHandler(RequestFault response);
+typedef Object ErrorHandler(Object response);
 
 class ModelTransform<M> {
   Resource _resource;
@@ -19,7 +19,11 @@ class ModelTransform<M> {
   Future<M> findAll() {
     return _resource.findAll().then(_transform).catchError(_errorh);
   }
+
+  Future<M> delete(id) {
+    return _resource.delete(id).then(_transform).catchError(_errorh);
+  }
   
   M _transform(Object json) => _modelFactory(json);
-  M _errorh(RequestFault requestFault) => _errorHandler(requestFault);
+  M _errorh(Object requestFault) => _errorHandler(requestFault);
 }
